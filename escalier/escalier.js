c2d56d08565448dc5b431204601eff96c913ebde
Francois Christiaen <francois-christiaen@MacBook-Pro-de-Francois-166.local>
2020-06-01 23:50:41 +0200

    35 minutes BAM !

--- escalier/escalier.js
+++ escalier/escalier.js
@@ -1,1 +1,27 @@
-alert("Super escalier");
+var myStairs = process.argv[2];
+function stairs(stairs = 0){
+    if(stairs == 0){
+        console.log("Pour atteindre le sommet il te faut viser haut!");
+    }else{
+        for(var i = 1; i <= stairs; i++){
+            var spaceInt = stairs - i,
+                stepsInt = i,
+                step = "",
+                j = 1,
+                k = 1;
+
+            // Spaces
+            while(j <= spaceInt){
+                step += " ";
+                j++;
+            };
+            // Steps
+            while(k <= stepsInt){
+                step += "#";
+                k++;
+            }
+            console.log(step);
+        }
+    }
+}
+stairs(myStairs);
